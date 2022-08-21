99127dd2266bbb35e71e68b2ca6a9764b5e4f490
AlexeySivakov <alexeysivakov88@gmail.com>
2022-08-21 18:45:17 +0300

    Add files via upload

---
+++ bet/BetException.java
@@ -0,0 +1,10 @@
+package bet;
+
+public class BetException extends Exception {
+    String text = "Введено некорректное значение! Повторите ввод:";
+
+    @Override
+    public String getMessage() {
+        return text;
+    }
+}
