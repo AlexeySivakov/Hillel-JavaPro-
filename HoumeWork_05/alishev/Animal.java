efc146e4feca856f4c9f3e349ffaf730e5374e01
AlexeySivakov <alexeysivakov88@gmail.com>
2022-08-21 17:44:46 +0300

    Add files via upload

---
+++ HoumeWork_05/alishev/Animal.java
@@ -0,0 +1,13 @@
+package alishev;
+
+public class Animal {
+    String name =  "Some name";
+
+    public void eat(){
+        System.out.println("I`m eating!");
+    }
+
+    public void sleep(){
+        System.out.println("I`m sleeping!");
+    }
+}
